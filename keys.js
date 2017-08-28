a922f28aa71f78fe69c392da0bb37816085c61fb
Ash Maher <ashkanmaherbus@gmail.com>
2017-08-27 19:44:03 -0700

    initialized package.json file, made .gitignore file and added lines instructed, made keys.js and applied twitter api keys.

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: ye3mJdpvye8R2Yp5r975Ur1HV,
+  consumer_secret: 	Eu1UTe7tuhMnEMY0JLTz9x9sDQ7jTLntl1oqgZcicTPy42Y6Nw
+,
+  access_token_key: 901955012095483904-FUnlDYgGvxdaf3KbQxwuWsWFLuVWUWa,
+  access_token_secret: 7Cc7A621Id3vxOUcY1MKALjCQK5Nz8iuJLUcopUVrWxom
+,
+}
