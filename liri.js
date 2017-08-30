55af030dbbafce7a94985d8c3ead0b9b3069a966
Ash Maher <ashkanmaherbus@gmail.com>
2017-08-29 18:30:28 -0700

    This is how far I was able to take this

--- liri.js
+++ liri.js
@@ -1,97 +1,86 @@
 //Code needed to grab the data I need from keys.js
 var keys = require("./keys.js");
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require("request");
+var fs = require("fs");
+
 // new indexes
-var liriCommands = process.argv[2];
-var liriSearch = process.argv[3];
-for (var i = 4; i < process.argv.length; i++) {
+var liriCommands = process.argv[3];
+var liriSearch = process.argv[4];
+/*for (var i = 4; i < process.argv.length; i++) {
   liriSearch += " " + process.argv[i];
-}
+}*/
 
 
-function userCommands() {
+function userCommands(command) {
   if (liriCommands === "my-tweets"){
-    runTweets();
+
+     var userName = "LildickyTom";
+     var client = new Twitter(keys.twitterKeys);
+
+    var params = {screen_name: userName};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
+        console.log(tweets[0].text);
+
+      }
+    });
+  }
+}
+   if (liriCommands === "spotify-this-song"){
+    function runSong() {
+      var song = 'The Sign';
+      if (liriSearch) {
+        song = liriSearch;
+      }
+
+      var spotify = new Spotify({
+        id: keys.spotifyKeys.id,
+        secret: keys.spotifyKeys.secret
+      });
+
+      spotify.search({ type: 'track', query: song }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        console.log(JSON.parse(data, null, 2));
+    })
+    }
   }
 
-  else if (liriCommands === "spotify-this-song"){
-    runSong();
-  }
+  if (liriCommands === "movie-this") {
+    var movie = "Mr. Nobody";
+    if(liriSearch) {
+      movie = liriSearch;
 
-  else if (liriCommands === "movie-this") {
-    runMovie();
-  }
+    request("http://www.omdbapi.com/?apikey=" + keys.omdbKey.api + "&t", function(error, response, body) {
+      if (!error && response.statusCode === 200) {
 
+        // Parse the body of the site and recover just the imdbRating
+
+        console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+      }
+      });
+
+
+  }
+}
   else {
     console.log("error")
   }
-}
+
 
 
  // Function that pulls tweets and displays when commanded
-function runTweets() {
- var userName = "LildickyTom";
- if (liriSearch) {
-   userName = liriSearch;
- }
- var Twitter = require('twitter');
 
-  var client = new Twitter({
-      consumer_key: keys.twitterKeys.consumer_key,
-      consumer_secret: keys.twitterKeys.consumer_secret,
-      access_token_key: keys.twitterKeys.access_token_key,
-      access_token_secret: keys.twitterKeys.access_token_secret
-    });
 
-var params = {screen_name: 'userName'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
 
-}
 
-function runSong() {
-  var song = 'The Sign';
-  if (liriSearch) {
-    song = liriSearch;
-  }
-  var Spotify = require('node-spotify-api');
 
-  var spotify = new Spotify({
-    id: keys.spotifyKeys.id,
-    secret: keys.spotifyKeys.secret
-  });
 
-  spotify.search({ type: 'track', query: song }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-    console.log(data);
-})
-}
 
-function runMovie() {
-  var movie = "Mr. Nobody";
-  if(liriSearch) {
-    movie = liriSearch;
-  }
-}
-
-var request = require("request");
-
-request("http://www.omdbapi.com/?apikey=" + keys.omdbKey.api + "&t", function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-
-    // Parse the body of the site and recover just the imdbRating
-
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-  }
-  });
-
-var fs = require("fs");
-
-fs.readFile("random.txt", "utf8", function(error, data) {
+if (liriCommand === "do-what-it-says") { fs.readFile("random.txt", "utf8", function(error, data) {
 
   if (error) {
     return console.log(error);
@@ -99,9 +88,5 @@ fs.readFile("random.txt", "utf8", function(error, data) {
     console.log(data);
     var dataArr = data.split(",");
 });
-
-
-
-
-
+}
 userCommands();
